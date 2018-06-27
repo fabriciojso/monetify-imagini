bb0bf92dd93f79334bd07fe11aa7fde2e76e69ff
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-06-27 05:27:05 -0300

    Completando testes

--- src/core/constructor/Picture.test.ts
+++ src/core/constructor/Picture.test.ts
@@ -1,23 +1,29 @@
-import { readFileSync } from 'fs';
-import { pictureGroupComponent01, pictureGroupComponent02 } from '../component/GroupComponent.test';
+import { compareImages, isSame } from '@bouzuya/compare-images';
+import { unlinkSync } from 'fs';
+import * as Jimp from 'jimp';
+import { pictureGroupComponent01 } from '../component/GroupComponent.test';
 import { userExemple } from '../interface/IUser.test';
 import { background } from './Background.test';
 import { Picture } from './Picture';
 
-describe('Inserir fotos do usuário na imagem', () => {
-  test('Valida a classe Picture -- Exemplo 01', async () => {
-    const backgroundTemp = await background.run();
-    const picture = new Picture(backgroundTemp, userExemple, pictureGroupComponent01);
-    const pictureTemp = await picture.run();
-    const status = pictureTemp.bitmap.data.compare(readFileSync('resources/tests/applications/01/result2.png'));
-    expect(status).toEqual(1);
-  });
+const tmpImage = 'resources/tests/tmp/exemplo1.png';
+const originalImage = 'resources/tests/applications/01/result2.png';
 
-  test('Valida a classe Picture -- Exemplo 02', async () => {
-    const backgroundTemp = await background.run();
-    const picture = new Picture(backgroundTemp, userExemple, pictureGroupComponent02);
-    const pictureTemp = await picture.run();
-    const status = pictureTemp.bitmap.data.compare(readFileSync('resources/tests/applications/01/result1.png'));
-    expect(status).toEqual(1);
-  });
+describe('Inserir fotos do usuário na imagem', () => {
+  test(
+    'Valida a classe Picture -- Exemplo 01',
+    async () => {
+      const backgroundTemp = await background.run();
+      const picture = new Picture(backgroundTemp, userExemple, pictureGroupComponent01);
+      await picture
+        .run()
+        .then(image => new Promise((res, rej) => image.write(tmpImage, (err, ret) => (err ? rej(err) : res(ret)))));
+      const tmp = await Jimp.read(tmpImage);
+      const original = await Jimp.read(originalImage);
+      const result = compareImages(tmp.bitmap, original.bitmap);
+      unlinkSync(tmpImage);
+      expect(isSame(result)).toEqual(true);
+    },
+    30000
+  );
 });
